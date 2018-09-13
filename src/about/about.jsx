1a6ae30ce5ac192eb87ccb8ca2cfabc85106ac15
Bruno Arcanjo <bm.arcanjo@gmail.com>
2018-09-13 18:04:45 -0300

    Primeira versao do projeto sem Redux

---
+++ src/about/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PageHeader from '../template/pageHeader';
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós' />
+
+        <h2>História</h2>
+        <p>Aplicação desenvolvida através do curso de React na Udemy!</p>
+    </div>
+)
---
+++ src/todo/todoForm.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Grid from '../template/grid';
+import IconButton from '../template/iconButton';
+
+export default props => {
+    const keyHandler = event => {
+        if (event.key === 'Enter') {
+            event.shiftKey ? props.handleSearch() : props.handleAdd();
+        } else if (event.key === 'Escape') {
+            props.handleClear();
+        }
+    }
+
+    return (
+        <div role="form" className="todoForm">
+            <Grid cols="12 9 10">
+                <input
+                    type="text"
+                    id="description"
+                    className="form-control"
+                    placeholder="Adicione uma tarefa"
+                    onChange={props.handleChange}
+                    value={props.description}
+                    onKeyUp={keyHandler}
+                />
+            </Grid>
+            <Grid cols="12 3 2">
+                <IconButton style="primary" icon="plus" onClick={props.handleAdd} />
+                <IconButton style="info" icon="search" onClick={props.handleSearch} />
+                <IconButton style="default" icon="close" onClick={props.handleClear} />
+            </Grid>
+        </div>
+    )
+}
